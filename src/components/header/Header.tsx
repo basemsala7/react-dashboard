d24ea1df4d29209ad5b76ef91719bdf9b114a916
basemsala7 <basembebo809@github.com>
2024-02-24 21:08:43 +0200

    after edite image path

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,13 @@ import "./header.css";
 import { IoIosSearch } from "react-icons/io";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
-
+import logo from "../../../public/logo.svg";
+import adminAvatar from "../../../public/pexels-photo-11038549.jpeg";
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <img src="../../../public/logo.svg" alt="logo" />
+        <img src={logo} alt="logo" />
         <span>dashboard</span>
       </div>
 
@@ -18,12 +19,7 @@ const Header = () => {
           <span>1</span>
         </div>
         <div className="avaterWraper">
-          <img
-            src="../../../public/pexels-photo-11038549.jpeg"
-            alt="avater"
-            width={25}
-            height={25}
-          />
+          <img src={adminAvatar} alt="avater" width={25} height={25} />
           <span>Admin</span>
         </div>
         <IoSettingsOutline />
